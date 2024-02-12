6dd6d4cd6b0210bf4bf8d27a8d44b23b648d854e
Willow7T <williamlynne7@gmail.com>
2024-02-12 20:28:39 +0630

    Fix bug in login functionality

---
+++ resources/views/components/home-body.blade.php
@@ -0,0 +1,11 @@
+<div class="relative shadow-xl sm:rounded-lg mt-2 h-8 bg-white dark:bg-blue-700">
+  
+    {{-- <div class="bg-cover bg-center absolute inset-0" style="background-image: url('{{ asset('storage/background/bruh1.jpg') }}')">
+        <!-- Content inside the div -->
+
+    </div> --}}
+
+    {{-- <div class="mx-2 my-2">
+        <img class="opacity-25" src="{{ asset('storage/background/bruh1.jpg') }}"
+    </div> --}}
+</div>
