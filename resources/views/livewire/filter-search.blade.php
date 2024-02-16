c5b41d5e4757c6efcbf450a11f32f76c540f93b8
Willow7T <williamlynne7@gmail.com>
2024-02-16 17:31:59 +0630

    Pagination

--- resources/views/livewire/filter-search.blade.php
+++ resources/views/livewire/filter-search.blade.php
@@ -1,107 +1,103 @@
 <div>
-    {{-- <div class="fixed"> --}}
-    
-    <div class="2xl:fixed z-50">
-        <form id="searchForm" wire:submit.prevent="$refresh" >
-            <div class="flex flex-row gap-x-4 p-2">
-                <input class="border-gray-100 border-2 dark:bg-slate-800 dark:text-slate-100 dark:border-slate-200 
-                focus:outline-none focus:border-blue-800 focus:ring-blue-800 dark:focus:border-purple-600 dark:focus:ring-purple-600
-                 hover:border-blue-600 rounded-md sm:text-sm dark:hover:border-purple-600
-                " type="text" wire:model="search" placeholder="Search by title">
-                <x-button type="submit">
-                    Search
-                </x-button>
-            </div>                 
-            <div class="flex flex-row gap-x-6 pl-2">    
-                <div class="w-max-24 w-32 h-5 px-4 py-6  ">
-                    <h3 class="-mx-2 -my-3 flow-root">
-                        <!-- Expand/collapse section button -->
-                            <button wire:click="toggleMonth" type="button" class="
-                            flex w-full items-center justify-between bg-white px-2 dark:bg-slate-800 text-gray-400 
-                            hover:text-gray-500 
-                            "aria-controls="filter-section-mobile-0">
-                                <span class="font-medium text-gray-900  dark:text-gray-100">Month</span>
-                                <span class="ml-6 flex items-center">
-                                    <!-- Expand icon, show/hide based on section open state. -->
-                                    <svg class="h-5 w-5 expand-icon" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-                                    </svg>
-                                    <!-- Collapse icon, show/hide based on section open state. -->
-                                    <svg class="h-5 w-5 collapse-icon" style="display: none;" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z" clip-rule="evenodd" />
-                                    </svg>
-                                </span>
-                            </button>
-                    </h3>
-                    <!-- Filter section, show/hide based on section state. -->
-                    <div class="pt-8 " id="filter-section-mobile-0" style="display: {{ $isMonthExpanded ? 'block' : 'none' }}">
-                        <div class="space-y-6 rounded bg-gray-200 dark:bg-slate-700 p-2">
-                            @foreach ($monthList as $m)
-                            <div class="flex items-center">
-                                <input id="filter-mobile-color-{{ $loop->index }}" type="checkbox" wire:model.live="months" value="{{ $m }}"  class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                <label for="filter-mobile-color-{{ $loop->index }}" class="ml-3 min-w-0 flex-1 text-gray-500 dark:text-gray-100">{{ $m }}</label>
-                            </div>
-                             @endforeach
-                        </div>
-                    </div>
-                </div>    
-                <div class="w-max-24 w-32 h-5 px-4 py-6  ">
-                    <h3 class="-mx-2 -my-3 flow-root">
+    {{-- <div class="fixed"> --}}   
+        <div class="2xl:fixed left-4 top-32 z-50 block md:flex sm:flex">
+            <form id="searchForm" wire:submit.prevent="$refresh"   class="z-10">
+                <div class="flex flex-row gap-x-4 p-2">
+                    <input class="border-gray-100 border-2 dark:bg-slate-800 dark:text-slate-100 dark:border-slate-200 
+                    focus:outline-none focus:border-blue-800 focus:ring-blue-800 dark:focus:border-purple-600 dark:focus:ring-purple-600
+                     hover:border-blue-600 rounded-md sm:text-sm dark:hover:border-purple-600
+                    " type="text" wire:model="search" placeholder="Search by title">
+                    <x-button type="submit">
+                        Search
+                    </x-button>
+                </div>                 
+                <div class="flex flex-row gap-x-6 pl-2">    
+                    <div class="w-max-24 w-32 h-5 px-4 py-6  ">
+                        <h3 class="-mx-2 -my-3 flow-root">
                             <!-- Expand/collapse section button -->
-                            <button wire:click="toggleYear" type="button" class="
-                            flex w-full items-center justify-between bg-white dark:bg-slate-800 px-2  text-gray-400 
-                            hover:text-gray-500 
-                            "aria-controls="filter-section-mobile-1">
-                                <span class="font-medium text-gray-900 dark:text-gray-100">Year</span>
-                                <span class="ml-6 flex items-center">
-                                    <!-- Expand icon, show/hide based on section open state. -->
-                                    <svg class="h-5 w-5 expand-icon" viewBox="0 0 20 20" fill="currentColor">
-                                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-                                    </svg>
-                                    <!-- Collapse icon, show/hide based on section open state. -->
-                                    <svg class="h-5 w-5 collapse-icon" style="display: none;" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z" clip-rule="evenodd" />
-                                    </svg>
-                                </span>
-                            </button> 
-                    </h3>
-                    <!-- Filter section, show/hide based on section state. -->
-                    <div class="pt-10" id="filter-section-mobile-1" style="display: {{ $isYearExpanded ? 'block' : 'none' }}">
-                        <div class="space-y-6 rounded bg-gray-200 dark:bg-slate-700 p-2">
-                            @foreach ($yearList as $y)
-                            <div class="flex items-center">
-                                <input id="filter-mobile-color-{{ $loop->index }}" type="checkbox" wire:model.live="years" value="{{ $y }}" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 ">
-                                <label for="filter-mobile-color-{{ $loop->index }}" class="ml-3 min-w-0 flex-1 text-gray-500 dark:text-gray-100">{{ $y }}</label>
+                                <button wire:click="toggleMonth" type="button" class="
+                                flex w-full items-center justify-between bg-white px-2 dark:bg-slate-800 text-gray-400 
+                                hover:text-gray-500 
+                                "aria-controls="filter-section-mobile-0">
+                                    <span class="font-medium text-gray-900  dark:text-gray-100">Month</span>
+                                    <span class="ml-6 flex items-center">
+                                        <!-- Expand icon, show/hide based on section open state. -->
+                                        <svg class="h-5 w-5 expand-icon" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                                        </svg>
+                                        <!-- Collapse icon, show/hide based on section open state. -->
+                                        <svg class="h-5 w-5 collapse-icon" style="display: none;" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z" clip-rule="evenodd" />
+                                        </svg>
+                                    </span>
+                                </button>
+                        </h3>
+                        <!-- Filter section, show/hide based on section state. -->
+                        <div class="pt-8 " id="filter-section-mobile-0" style="display: {{ $isMonthExpanded ? 'block' : 'none' }}">
+                            <div class="space-y-6 rounded bg-gray-200 dark:bg-slate-700 p-2">
+                                @foreach ($monthList as $m)
+                                <div class="flex items-center">
+                                    <input id="filter-mobile-color-{{ $loop->index }}" type="checkbox" wire:model.live="months" value="{{ $m }}"  class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
+                                    <label for="filter-mobile-color-{{ $loop->index }}" class="ml-3 min-w-0 flex-1 text-gray-500 dark:text-gray-100">{{ $m }}</label>
+                                </div>
+                                 @endforeach
                             </div>
-                            @endforeach
+                        </div>
+                    </div>    
+                    <div class="w-max-24 w-32 h-5 px-4 py-6  ">
+                        <h3 class="-mx-2 -my-3 flow-root">
+                                <!-- Expand/collapse section button -->
+                                <button wire:click="toggleYear" type="button" class="
+                                flex w-full items-center justify-between bg-white dark:bg-slate-800 px-2  text-gray-400 
+                                hover:text-gray-500 
+                                "aria-controls="filter-section-mobile-1">
+                                    <span class="font-medium text-gray-900 dark:text-gray-100">Year</span>
+                                    <span class="ml-6 flex items-center">
+                                        <!-- Expand icon, show/hide based on section open state. -->
+                                        <svg class="h-5 w-5 expand-icon" viewBox="0 0 20 20" fill="currentColor">
+                                            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                                        </svg>
+                                        <!-- Collapse icon, show/hide based on section open state. -->
+                                        <svg class="h-5 w-5 collapse-icon" style="display: none;" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z" clip-rule="evenodd" />
+                                        </svg>
+                                    </span>
+                                </button> 
+                        </h3>
+                        <!-- Filter section, show/hide based on section state. -->
+                        <div class="pt-10" id="filter-section-mobile-1" style="display: {{ $isYearExpanded ? 'block' : 'none' }}">
+                            <div class="space-y-6 rounded bg-gray-200 dark:bg-slate-700 p-2">
+                                @foreach ($yearList as $y)
+                                <div class="flex items-center">
+                                    <input id="filter-mobile-color-{{ $loop->index }}" type="checkbox" wire:model.live="years" value="{{ $y }}" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 ">
+                                    <label for="filter-mobile-color-{{ $loop->index }}" class="ml-3 min-w-0 flex-1 text-gray-500 dark:text-gray-100">{{ $y }}</label>
+                                </div>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
-     </div>
-    
-    <div class="py-8">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-gray-200 flex flex-col shadow-xl sm:rounded-lg">
-                @foreach ($articles as $article)
-                <a href="{{ route('article.show', ['id' => $article->id, 'title' => Str::slug($article->title)]) }}" class="">
-                    <div class="bg-cyan-500 m-4 p-4 rounded">
-                          <h2>{{ $article->title }}</h2>
-                            <p>{{ $article->intro }}</p>
-                            @if($article->image)
-                                <img class="max-w-xs max-h-64" src="{{asset('storage/' . $article->image)  }}" alt="">
-                            @else
-                                <img class="max-w-xs max-h-64" src="{{ asset('background/blank.png') }}" alt="Blank image">
-                            @endif
-                    </div>
-                </a>
-                @endforeach
+            </form>
+        </div>
+
+        <div class="py-8">
+            <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-gray-200 flex flex-col shadow-xl sm:rounded-lg">
+                    @foreach ($articles as $article)
+                    <a href="{{ route('article.show', ['id' => $article->id, 'title' => Str::slug($article->title)]) }}" class="">
+                        <div class="bg-cyan-500 m-4 p-4 rounded">
+                              <h2>{{ $article->title }}</h2>
+                                <p>{{ $article->intro }}</p>
+                                @if($article->image)
+                                    <img class="max-w-xs max-h-64" src="{{asset('storage/' . $article->image)  }}" alt="">
+                                @else
+                                    <img class="max-w-xs max-h-64" src="{{ asset('background/blank.png') }}" alt="Blank image">
+                                @endif
+                        </div>
+                    </a>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-
-    
-
-    {{ $articles->links() }}
+    {{ $articles->links("pagination-links") }}
 </div>
--- resources/views/pagination-links.blade.php
+++ resources/views/pagination-links.blade.php
@@ -1,4 +1,66 @@
-@if ($pagination->hasPages())
-    <li wire:click="previousPage()"  class="pagination
-    
+<div>
+
+    @if ($paginator->hasPages())
+    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="xl:absolute left-2 bottom-3 p-4 flex items-center gap-x-2">
+        <div>
+            @if ($paginator->onFirstPage())
+                <span class="
+                relative inline-flex items-center px-4 py-2 
+                text-sm font-medium text-gray-500 bg-white border 
+                border-gray-300 cursor-default leading-5 rounded-md">
+                    {!! __('Previous') !!}
+                </span>
+            @else
+                <a href="{{ $paginator->previousPageUrl() }}" class="
+                    relative inline-flex items-center px-4 py-2 text-sm 
+                    font-medium text-gray-700 bg-white border 
+                    border-gray-300 leading-5 hover:text-gray-500 
+                    focus:z-10 focus:outline-none focus:border-blue-300 
+                    focus:ring-1 focus:ring-blue-300 focus:ring-opacity-5 rounded-md">
+                    {!! __('Previous') !!}
+                </a>
+            @endif
+        </div>
+
+        <div>
+            @foreach (range(1, $paginator->lastPage()) as $page)
+                @if ($page == $paginator->currentPage())
+                    <span class="relative inline-flex items-center 
+                    px-4 py-2 text-sm font-medium text-gray-500 
+                    bg-white border border-gray-300 
+                    cursor-default leading-5 rounded-md">
+                        {{ $page }}
+                    </span>
+                @else
+                    <a href="{{ $paginator->url($page) }}" class="
+                        relative inline-flex items-center 
+                        px-4 py-2 text-sm font-medium 
+                        text-gray-700 bg-white border border-gray-300 
+                        leading-5 hover:text-gray-500 
+                        focus:z-10 focus:outline-none focus:border-blue-300 
+                        focus:ring-1 focus:ring-blue-300 focus:ring-opacity-5 rounded-md">
+                        {{ $page }}
+                    </a>
+                @endif
+            @endforeach
+        </div>
+
+        <div>
+            @if ($paginator->hasMorePages())
+                <a href="{{ $paginator->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:ring-1 focus:ring-blue-300 focus:ring-opacity-5 rounded-md">
+                    {!! __('Next') !!}
+                </a>
+            @else
+                <span class="
+                relative inline-flex items-center px-4 py-2 
+                text-sm font-medium text-gray-500 
+                bg-white border border-gray-300 
+                cursor-default leading-5 rounded-md">
+                    {!! __('Next') !!}
+                </span>
+            @endif
+        </div>
+    </nav>
 @endif
+
+</div>
