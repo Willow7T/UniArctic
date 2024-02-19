5cafd03ec4f67ffcce9d28ee12a61b40493cab6a
Willow7T <williamlynne7@gmail.com>
2024-02-19 14:46:57 +0630

    Update dependencies and fix asset paths

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,10 +11,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       <!-- Scripts -->
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <script src="{{ secure_asset('build/assets/app-DgbtYFve.js') }}"></script>
 
         <!-- Styles -->
+        <link href="{{ secure_asset('build/assets/app-BE0mZvCE.css') }}" rel="stylesheet">
         @livewireStyles
     </head>
     <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,21 +9,23 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-       
-
-  <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
         
 
+        <!-- Scripts -->
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <script src="{{ secure_asset('build/assets/app-DgbtYFve.js') }}"></script>
+
         <!-- Styles -->
+        <link href="{{ secure_asset('build/assets/app-BE0mZvCE.css') }}" rel="stylesheet">
         @livewireStyles
-        <!-- Styles -->
         <style>
             .bg-dots-darker{background-image:url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E")}
             .dark\:bg-dots-lighter{background-image:url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E")}
+        
+        
         </style>
     </head>
-    <body class="antialiased">
+    <body class="antialiased newer">
         {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 selection:bg-red-500 selection:text-white" style="background-image: url('{{ asset('storage/background/uniback.jpg') }}')"> --}}
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             <section>
