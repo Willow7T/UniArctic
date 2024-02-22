ef423e6159aa6d28cf490e61c4c9ce547b9b1421
Kuro <kmh61030@gmail.com>
2024-02-22 18:44:53 +0630

    Changes UI for admin

--- resources/views/livewire/admin-statpanel.blade.php
+++ resources/views/livewire/admin-statpanel.blade.php
@@ -9,17 +9,17 @@
             <table class="m-3 table-fixed border-collapse border border-slate-500 w-11/12">
                 <thead>
                     <tr class="h-10">
-                        <th class="border border-slate-600">Article</th>
-                        <th class="border border-slate-600">Author</th>
-                        <th class="border border-slate-600">Views</th>
+                        <th class="border border-slate-600 backdrop-blur-sm p-4">Article</th>
+                        <th class="border border-slate-600 backdrop-blur-sm p-4">Author</th>
+                        <th class="border border-slate-600 backdrop-blur-sm p-4">Views</th>
                     </tr>
                 </thead>
                 <tbody >
                     @foreach($articles as $article)
                     <tr class="h-10">
-                        <td class="border border-slate-600 text-left">{{ $article->title }}</td>
-                        <td class="border border-slate-600 text-center">{{ optional($article->author)->name ?? 'Deleted User' }}</td>
-                        <td class="border border-slate-600 text-right">{{ $article->views_count }} views</td>
+                        <td class="border border-slate-600 backdrop-blur-sm p-4 text-left">{{ $article->title }}</td>
+                        <td class="border border-slate-600 backdrop-blur-sm p-4 text-center">{{ optional($article->author)->name ?? 'Deleted User' }}</td>
+                        <td class="border border-slate-600 backdrop-blur-sm p-4 text-right">{{ $article->views_count }} views</td>
                     </tr>
                     @endforeach
                 </tbody>
@@ -34,20 +34,20 @@
             <table class="m-3 table-fixed border-collapse border border-slate-500 w-11/12">
                 <thead>
                      <tr class="h-10">
-                       <th class="border border-slate-600">Author</th>
-                       <th class="border border-slate-600">Articles</th>
+                       <th class="border border-slate-600 p-4 backdrop-blur-sm">Author</th>
+                       <th class="border border-slate-600 p-4 backdrop-blur-sm">Articles</th>
                      </tr>
                 </thead>
                 <tbody >
                    @foreach($authors as $author)
                    <tr>
                         {{-- <p>{{ $article->anonymous ? "Anonymous" : $article->author->name  }}</p> --}}
-                        <td class="border border-slate-600 text-left">{{ optional($author)->name ?? 'Deleted User' }}</td>
-                        <td class="border border-slate-600 text-right">{{ $author->articles_count }}</td>
+                        <td class="border border-slate-600 text-left p-4 backdrop-blur-sm">{{ optional($author)->name ?? 'Deleted User' }}</td>
+                        <td class="border border-slate-600 text-right p-4 backdrop-blur-sm">{{ $author->articles_count }}</td>
                    </tr>
                    @endforeach
-                        <td class="border border-slate-600 text-left">Deleted Author"s"</td>
-                        <td class="border border-slate-600 text-right">{{ $deleted_authors[0]->counters}}</td>
+                        <td class="border border-slate-600 text-left p-4 backdrop-blur-sm">Deleted Author"s"</td>
+                        <td class="border border-slate-600 text-right p-4 backdrop-blur-sm">{{ $deleted_authors[0]->counters}}</td>
                 </tbody>
           </table>
         </div>
